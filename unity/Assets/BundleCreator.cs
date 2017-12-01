d0c693fef8c6a0774093544dcd0f90c38ce088ac
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2017-12-01 21:25:39 +0100

    Added Unity multiplayer mod asset bundle project.

---
+++ unity/Assets/BundleCreator.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEditor;
+using System.IO;
+
+public class BundleCreator {
+
+	[MenuItem("MSCMP/Build Asset Bundles")]
+	static void BuildABs() {
+		BuildPipeline.BuildAssetBundles("../data", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows);
+	}
+}
