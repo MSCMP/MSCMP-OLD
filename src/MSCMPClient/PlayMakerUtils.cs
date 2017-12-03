e74aabf3ce0824ed4705781a239408f23665ba60
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2017-12-03 19:33:27 +0100

    Added play maker utils class.

---
+++ src/MSCMPClient/PlayMakerUtils.cs
@@ -0,0 +1,48 @@
+﻿using HutongGames.PlayMaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCMP {
+	class PlayMakerUtils {
+
+
+		/// <summary>
+		/// Add new global transition from the given event to the state name to the given PlayMaker FSM.
+		/// </summary>
+		/// <param name="fsm">The PlayMaker FSM to add global transition for.</param>
+		/// <param name="ev">The event triggering the transition.</param>
+		/// <param name="stateName">The state this transition activates.</param>
+		static public void AddNewGlobalTransition(PlayMakerFSM fsm, FsmEvent ev, string stateName) {
+			FsmTransition[] oldTransitions = fsm.FsmGlobalTransitions;
+			List<FsmTransition> temp = new List<FsmTransition>();
+			foreach (FsmTransition t in oldTransitions) {
+				temp.Add(t);
+			}
+			FsmTransition transition = new FsmTransition();
+			transition.FsmEvent = ev;
+			transition.ToState = stateName;
+			temp.Add(transition);
+
+			fsm.Fsm.GlobalTransitions = temp.ToArray();
+		}
+
+
+		/// <summary>
+		/// Add new action into play maker state.
+		/// </summary>
+		/// <param name="state">The state to add action to.</param>
+		/// <param name="action">The action to add.</param>
+		static public void AddNewAction(FsmState state, FsmStateAction action) {
+			FsmStateAction[] oldActions = state.Actions;
+			List<FsmStateAction> temp = new List<FsmStateAction>();
+			temp.Add(action);
+			foreach (var v in oldActions) {
+				temp.Add(v);
+			}
+			state.Actions = temp.ToArray();
+		}
+	}
+}
