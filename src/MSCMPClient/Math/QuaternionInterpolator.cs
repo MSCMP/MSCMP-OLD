86818a5845574d56da76066e7f73beff76d83fe1
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2017-12-04 00:47:45 +0100

    Added interpolators.

---
+++ src/MSCMPClient/Math/QuaternionInterpolator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MSCMP.Math {
+	class QuaternionInterpolator {
+
+		Quaternion current = new Quaternion();
+		Quaternion source = new Quaternion();
+		Quaternion target = new Quaternion();
+
+		public void SetTarget(Quaternion quat) {
+			source = current;
+			target = quat;
+		}
+
+		public void Teleport(Quaternion quat) {
+			current = source = target = quat;
+		}
+
+		public Quaternion Evaluate(float alpha) {
+			current = Quaternion.Slerp(source, target, alpha);
+			return current;
+		}
+	}
+}
