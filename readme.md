dd7e839600908d5a405321c1dd4c6cfb0be962f1
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2017-12-06 21:04:54 +0100

    Added workspace setup readme file and updated setup.bat.

---
+++ readme.md
@@ -0,0 +1,28 @@
+# My Summer Car Multiplayer Mod
+
+## How to work on the mod?
+
+### 1. Setup workspace.
+
+Before you start working on the mod make sure you properly setup your workspace. To do that run `setup.bat` file it will setup everything for you.
+
+### 2. Launch Visual Studio 2015 solution.
+
+Launch the mod solution placed in `src` folder.
+
+### 3. Generate network messages.
+
+First of all compile and run `MSCMPMessages` app. It will generate network messages file network code requires to compile.
+
+### 4. Play & develop!
+
+That's all. Now you can go to the `bin\Debug` folder and launch mod via `MSCMP.exe` executable. (Or `bin/Release` depending which configuration you are using)
+
+
+## Copyright
+
+Copyright (C) 2017 by Eryk Dwornicki All Rights Reserved
+
+
+
+
--- setup.bat
+++ setup.bat
@@ -1,15 +1,35 @@
-REM Windows script to setup workspace.
-
 @echo off
+
+:msc_path
+
+set /p GAME_PATH=Paste here path to my summer car without traling slash:
+
+if not exist "%GAME_PATH%/mysummercar.exe" goto invalid_path
+
+setx /m MSCMP_GAME_PATH "%GAME_PATH%"
+
+echo My Summer Car path has been written.
+
+echo Preparing build folder structure.
 
 if not exist bin mkdir bin
 
 if not exist bin\Release mkdir bin\Release
 copy 3rdparty\steamapi\steam_api.dll bin\Release
 copy data\steam_appid.txt bin\Release
+echo Release prepared.
 
 if not exist bin\Debug mkdir bin\Debug
 copy 3rdparty\steamapi\steam_api.dll bin\Debug
 copy data\steam_appid.txt bin\Debug
+echo Debug prepared.
 
 echo Workspace has been setup
+pause
+
+goto :eof
+
+:invalid_path
+
+echo Invalid my summer car path. Unable to find %GAME_PATH%/mysummercar.exe.
+goto msc_path
