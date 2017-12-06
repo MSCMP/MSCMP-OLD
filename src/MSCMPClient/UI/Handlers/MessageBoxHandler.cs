ca8fbca9832ff3721f6d099b124079c382b78762
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2017-12-06 19:32:40 +0100

    Added MPGUI.

---
+++ src/MSCMPClient/UI/Handlers/MessageBoxHandler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MSCMP.UI.Handlers {
+	/// <summary>
+	/// Handle of the message box window.
+	/// </summary>
+	class MessageBoxHandler : MonoBehaviour {
+
+		/// <summary>
+		/// Delegate of the callback called when window is closed.
+		/// </summary>
+		public delegate void OnClose();
+
+		OnClose onClose = null;
+
+		/// <summary>
+		/// Setup the handler after message box spawn.
+		/// </summary>
+		void Start() {
+			var btn = transform.FindChild("OKButton").GetComponent<Button>();
+			btn.onClick.AddListener(() => {
+				Close();
+
+				if (onClose != null) {
+					onClose();
+					onClose = null;
+				}
+			});
+		}
+
+		/// <summary>
+		/// Close currently active message box.
+		/// </summary>
+		public void Close() {
+			gameObject.SetActive(false);
+			MPGUI.Instance.ShowCursor(false);
+		}
+
+		/// <summary>
+		/// Show message box with given text.
+		/// </summary>
+		/// <param name="text">The text to show.</param>
+		/// <param name="onClose">The callback that will be closed when OK button is pressed.</param>
+		/// <returns>true if message box was showed false otherwise</returns>
+		public bool Show(string text, OnClose onClose = null) {
+			// Allow only one message box.
+			if (gameObject.activeSelf) {
+				return false;
+			}
+			this.onClose = onClose;
+			transform.FindChild("Text").gameObject.GetComponent<Text>().text = text;
+			gameObject.SetActive(true);
+			MPGUI.Instance.ShowCursor(true);
+			return true;
+		}
+	}
+}
---
+++ src/MSCMPClient/UI/MPGUI.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MSCMP.UI {
+	/// <summary>
+	/// Multiplayer mod GUI manager component.
+	/// </summary>
+	class MPGUI : MonoBehaviour {
+
+		/// <summary>
+		/// Instance of the GUI.
+		/// </summary>
+		public static MPGUI Instance = null;
+
+		/// <summary>
+		/// Message box handler.
+		/// </summary>
+		private Handlers.MessageBoxHandler messageBoxHandler = null;
+
+		public MPGUI() {
+			Instance = this;
+		}
+
+
+		/// <summary>
+		/// Setup UI.
+		/// </summary>
+		void Start() {
+			Utils.CallSafe("UISetup", () => {
+				var canvasPrefab = Client.LoadAsset<GameObject>("Assets/UI/UICanvas.prefab");
+
+				var canvas = Instantiate(canvasPrefab);
+				canvas.transform.SetParent(transform, false);
+
+				var messageBox = canvas.transform.FindChild("MessageBox").gameObject;
+				messageBoxHandler = messageBox.AddComponent<Handlers.MessageBoxHandler>();
+
+				DontDestroyOnLoad(gameObject);
+				DontDestroyOnLoad(canvas);
+				DontDestroyOnLoad(messageBox);
+			});
+		}
+
+		~MPGUI() {
+			Instance = null;
+		}
+
+
+		private int cursorCounter = 0;
+
+		/// <summary>
+		/// Show cursor.
+		/// </summary>
+		/// <param name="show">Should cursor be shown?</param>
+		public void ShowCursor(bool show) {
+			if (show) {
+				++cursorCounter;
+				if (!Cursor.visible) {
+					Cursor.visible = true;
+				}
+			}
+			else {
+				Client.Assert(cursorCounter > 0, "Tried to hide cursor too many times.");
+				if (--cursorCounter == 0) {
+					if (Application.loadedLevelName == "GAME") {
+						// Only hide cursor if we aren't in game.
+						Cursor.visible = false;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Show message box. (There can be maximum of one message box)
+		/// </summary>
+		/// <param name="text">The text to show.</param>
+		/// <param name="onClose">The callback to call when OK button is pressed.</param>
+		/// <returns>true if message box was shown false if there is already some message box and this one could not be showed.</returns>
+		public bool ShowMessageBox(string text, Handlers.MessageBoxHandler.OnClose onClose = null) {
+			return messageBoxHandler.Show(text, onClose);
+		}
+	}
+}
