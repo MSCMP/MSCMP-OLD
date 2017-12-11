c1f0ae16871db75dab4f0271272da077c2be3e47
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2017-12-11 04:10:01 +0100

    Added game callbacks that can be used to track various game events.

---
+++ src/MSCMPClient/Game/GameCallbacks.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace MSCMP.Game {
+	/// <summary>
+	/// Various callbacks called on game actions.
+	/// </summary>
+	static class GameCallbacks {
+		/// <summary>
+		/// Delegate of callback called when local player pickups object.
+		/// </summary>
+		/// <param name="gameObj">Picked up game object.</param>
+		public delegate void OnObjectPickup(GameObject gameObj);
+
+		/// <summary>
+		/// Callback called when local player pickups object.
+		/// </summary>
+		static public OnObjectPickup onObjectPickup;
+
+		/// <summary>
+		/// Delegate of callback called when local player pickups object.
+		/// </summary>
+		/// <param name="drop">Was the object dropped or throwed?</param>
+		public delegate void OnObjectRelease(bool drop);
+
+		/// <summary>
+		/// Callback called when local player releases object.
+		/// </summary>
+		static public OnObjectRelease onObjectRelease;
+
+		public delegate void OnLocalPlayerCreated();
+
+		/// <summary>
+		/// Callback called when local player spawns.
+		/// </summary>
+		static public OnLocalPlayerCreated onLocalPlayerCreated;
+
+		public delegate void OnWorldLoad();
+
+		/// <summary>
+		/// Callback called when game world gets loaded.
+		/// </summary>
+		static public OnWorldLoad onWorldLoad;
+
+		public delegate void OnWorldUnload();
+
+		/// <summary>
+		/// Callback called when game world gets unloaded.
+		/// </summary>
+		static public OnWorldUnload onWorldUnload;
+	}
+}
