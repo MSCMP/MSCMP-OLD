633545786dc40283d8217ae7050545b6f710f32d
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2018-01-22 20:28:02 +0100

    Added convenient getter of prefab descriptor to meta data component.

--- src/MSCMPClient/Game/Components/PickupableMetaDataComponent.cs
+++ src/MSCMPClient/Game/Components/PickupableMetaDataComponent.cs
@@ -5,6 +5,19 @@ namespace MSCMP.Game.Components {
 	/// Pickupable meta data component used to associate prefab/instances with prefab id.
 	/// </summary>
 	class PickupableMetaDataComponent : MonoBehaviour {
-		public int prefabId;
+		public int prefabId = -1;
+
+		/// <summary>
+		/// Getter for the prefab descriptor.
+		/// </summary>
+		public GamePickupableDatabase.PrefabDesc PrefabDescriptor
+		{
+			get {
+				if (prefabId != -1) {
+					return GamePickupableDatabase.Instance.GetPickupablePrefab(prefabId);
+				}
+				return null;
+			}
+		}
 	}
 }
