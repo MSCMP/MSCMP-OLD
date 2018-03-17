3d32e5cb32e5c6098a8d57365fd4be934253fc79
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2018-03-17 03:09:24 +0100

    Get rid of manually defined list of enums and messages to generate.
    A little time saver. :)

--- src/MSCMPMessages/Program.cs
+++ src/MSCMPMessages/Program.cs
@@ -1,36 +1,24 @@
-﻿namespace MSCMPMessages {
+using System;
+using System.Reflection;
+
+namespace MSCMPMessages {
 	class Program {
 		static void Main(string[] args) {
 			Generator generator = new Generator(@"..\..\src\MSCMPClient\Network\NetMessages.generated.cs");
 
-			generator.GenerateMessage(typeof(Messages.QuaternionMessage));
-			generator.GenerateMessage(typeof(Messages.Vector3Message));
-			generator.GenerateMessage(typeof(Messages.TransformMessage));
-			generator.GenerateMessage(typeof(Messages.HandshakeMessage));
-			generator.GenerateMessage(typeof(Messages.HeartbeatMessage));
-			generator.GenerateMessage(typeof(Messages.HeartbeatResponseMessage));
-			generator.GenerateMessage(typeof(Messages.DisconnectMessage));
-			generator.GenerateMessage(typeof(Messages.PickedUpSync));
-			generator.GenerateMessage(typeof(Messages.PlayerSyncMessage));
-			generator.GenerateMessage(typeof(Messages.OpenDoorsMessage));
-			generator.GenerateMessage(typeof(Messages.AskForWorldStateMessage));
-			generator.GenerateMessage(typeof(Messages.DoorsInitMessage));
-			generator.GenerateMessage(typeof(Messages.VehicleInitMessage));
-			generator.GenerateMessage(typeof(Messages.PickupableSpawnMessage));
-			generator.GenerateMessage(typeof(Messages.PickupableDestroyMessage));
-			generator.GenerateMessage(typeof(Messages.FullWorldSyncMessage));
-			generator.GenerateMessage(typeof(Messages.VehicleEnterMessage));
-			generator.GenerateMessage(typeof(Messages.VehicleLeaveMessage));
-			generator.GenerateMessage(typeof(Messages.VehicleSyncMessage));
-			generator.GenerateMessage(typeof(Messages.PickupObjectMessage));
-			generator.GenerateMessage(typeof(Messages.ReleaseObjectMessage));
-			generator.GenerateMessage(typeof(Messages.PickupableActivateMessage));
-			generator.GenerateMessage(typeof(Messages.PickupableSetPositionMessage));
-			generator.GenerateMessage(typeof(Messages.WorldPeriodicalUpdateMessage));
-			generator.GenerateMessage(typeof(Messages.RemoveBottleMessage));
-			generator.GenerateMessage(typeof(Messages.LightSwitchMessage));
-			generator.GenerateMessage(typeof(Messages.WeatherUpdateMessage));
-			generator.GenerateEnum(typeof(Messages.WeatherUpdateMessage.WeatherType));
+			Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+			foreach (var type in types) {
+				if (type.Namespace == null || type.Namespace != "MSCMPMessages.Messages") {
+					continue;
+				}
+
+				if (type.IsClass) {
+					generator.GenerateMessage(type);
+				}
+				else if (type.IsEnum) {
+					generator.GenerateEnum(type);
+				}
+			}
 			generator.EndGeneration();
 		}
 	}
