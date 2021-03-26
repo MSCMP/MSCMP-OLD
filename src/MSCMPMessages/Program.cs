550b3f4bd7f6d79c2ca0375a78281a58f92df24e
kirill.matsaberydze <kirill.matsaberydze@gmail.com>
2021-03-26 20:33:05 +0300

    Added autogeneration of NetMessages.generated.cs

--- src/MSCMPMessages/Program.cs
+++ src/MSCMPMessages/Program.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 namespace MSCMPMessages {
 	class Program {
 		static void Main(string[] args) {
-			Generator generator = new Generator(@"..\..\src\MSCMPClient\Network\NetMessages.generated.cs");
+			Generator generator = new Generator(String.Format(@"{0}\NetMessages.generated.cs", args[0]));
 
 			Type[] types = Assembly.GetExecutingAssembly().GetTypes();
 			foreach (var type in types) {
