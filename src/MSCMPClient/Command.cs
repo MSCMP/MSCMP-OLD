55110d925c1dfc2c971857f398e095d6bb736acb
kirill.matsaberydze <kirill.matsaberydze@gmail.com>
2021-04-21 22:14:11 +0300

    ModLoader interface inital

---
+++ src/MSCMPClient/Command.cs
@@ -0,0 +1,19 @@
+using MSCLoader;
+
+namespace MSCMP
+{
+		public class Command : ConsoleCommand
+		{
+				// What the player has to type into the console to execute your commnad
+				public override string Name => "example";
+
+				// The help that's displayed for your command when typing help
+				public override string Help => "Just type the command";
+
+				// The function that's called when the command is executed
+				public override void Run(string[] args)
+				{
+						ModConsole.Print("This command works!!!");
+				}
+		}
+}
